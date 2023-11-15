af8afbfc9d53a9088c376cb3bd593c780a5fa25b
jeeivan <jeeivansri@gmail.com>
2023-11-15 10:20:05 +0000

    Updated finch collector

--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Finch
+from .forms import FeedingForm
 
 # Create your views here.
-finches = [
-    {"species": "Galapagos Finch", "beak_length": 10.5, "beak_width": 3.2, "color": "brown"},
-    {"species": "Darwin's Finch", "beak_length": 8.7, "beak_width": 2.5, "color": "yellow"},
-    {"species": "Gouldian Finch", "beak_length": 12.0, "beak_width": 4.0, "color": "red"},
-    {"species": "Zebra Finch", "beak_length": 9.2, "beak_width": 2.8, "color": "black"},
-    {"species": "Society Finch", "beak_length": 11.1, "beak_width": 3.5, "color": "white"},
-    # Add more finch dictionaries as needed
-]
-
 def home(request):
     return render(request, 'home.html')
 
@@ -17,6 +11,37 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {
         'finches': finches
     })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    feeding_form = FeedingForm()
+    return render(request, 'finches/detail.html', {'finch': finch, 'feeding_form': feeding_form})
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+    # success_url = '/cats/{cat_id}'
+    # success_url = '/cats'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = '__all__'
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches'
+
+def add_feeding(request, finch_id):
+  # create a ModelForm instance using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
